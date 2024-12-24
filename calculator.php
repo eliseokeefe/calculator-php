47ed010d326932163848b258a02a9e67627eccbd
eliseokeefe <112436436+eliseokeefe@users.noreply.github.com>
2024-12-24 01:33:38 +0000

    switching IDEs for debugging

---
+++ calculator.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_GET['math'], $_GET['number1'], $_GET['number2'])) {
+    $x = $_GET['number1'];
+    $y = $_GET['number2'];
+    $op = $_GET['math'];
+
+    if (is_numeric($x) && is_numeric($y)) {
+        switch ($op) {
+            case 'addition':
+                $result = $x + $y;
+                break;
+            case 'subtraction':
+                $result = $x - $y;
+                break;
+            case 'multiplication':
+                $result = $x * $y;
+                break;
+            case 'division':
+                $result = ($y != 0) ? $x / $y : "Division by zero error!";
+                break;
+            default:
+                $result = "Invalid operation.";
+        }
+        echo is_numeric($result) ? (float)$result : $result;
+    } else {
+        echo "Can only do math for real numbers :/";
+    }
+} else {
+    echo "Missing required parameters.";
+}
+?>
+
+ 
